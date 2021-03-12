eb9daa690c615359653bad05b1b0ad90c5bfd59a
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-03-12 14:31:06 +0100

    definisco gli elementi e genero la funzione al click

---
+++ main.js
@@ -0,0 +1,19 @@
+$( function () {
+  //Variabili utili
+  var clickNext = $('.next');
+  var clickPrev = $('.prev');
+
+  clickNext.click(
+    function () {
+      alert();
+    }
+  );
+ clickPrev.click(
+   function () {
+     alert();
+   }
+ );
+
+
+}
+);

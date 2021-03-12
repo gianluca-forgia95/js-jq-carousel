2797bf65c18109333e8f9899be0c50e11ee2da01
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-03-12 15:24:25 +0100

    creo le funzioni per next e prev

--- main.js
+++ main.js
@@ -1,30 +1,43 @@
 $( function () {
-//Click su next
-var $next =  $('.next');
-var $prev = $('.prev');
- $next.click(
-  function() {
-    var imgAct = $('.images img.active');
-    imgAct.removeClass('active');
-    if ( imgAct.hasClass('last') == true ) {
-      $('.images img.first').addClass('active');
-    } else {
-      imgAct.next().addClass('active');
-    }
+//Funzioni utili
+function nextImg() {
+  var imgAct = $('.images img.active');
+  imgAct.removeClass('active');
+  if ( imgAct.hasClass('last') == true ) {
+    $('.images img.first').addClass('active');
+  } else {
+    imgAct.next().addClass('active');
   }
- );
+}
 
+function prevImg() {
+  var imgAct = $('.images img.active');
+  imgAct.removeClass('active');
+  if ( imgAct.hasClass('first') == true ) {
+    $('.images img.last').addClass('active');
+  } else {
+    imgAct.prev().addClass('active');
+  }
+}
+
+//variabili
+ var $next =  $('.next');
+ var $prev = $('.prev');
+
+//Click sul next
+ $next.click(
+   function () {
+    nextImg();
+   }
+);
+
+//Click sul prev
  $prev.click(
    function () {
-     var imgAct = $('.images img.active');
-     imgAct.removeClass('active');
-     if ( imgAct.hasClass('first') == true ) {
-       $('.images img.last').addClass('active');
-     } else {
-       imgAct.prev().addClass('active');
-     }
+     prevImg();
    }
- );
+);
+
 
 
 
